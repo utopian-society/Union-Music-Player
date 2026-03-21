3462347565a8c097c77ab82cfbe0c907349ab6ef
root <root@localhost.localdomain>
2026-03-21 08:22:17 +0000

    try auto and bug fixed

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,9 +5,9 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application") version "8.2.0"
+        id("com.android.application") version "8.6.0"
         id("com.android.library") version "8.2.0"
-        id("org.jetbrains.kotlin.android") version "1.9.22"
+        id("org.jetbrains.kotlin.android") version "2.0.20"
     }
 }
 dependencyResolutionManagement {
