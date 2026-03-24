f03b79b73149f3ef70a8a6190422c40b99b4af98
marcobibichan <bibichan2010@protonmail.com>
2026-03-24 22:23:41 +0800

    fix build issues

--- app/src/main/java/org/bibichan/union/player/ui/components/FloatingPlayer.kt
+++ app/src/main/java/org/bibichan/union/player/ui/components/FloatingPlayer.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
